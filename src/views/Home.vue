c0e6638119f77010480ec028dceece048bd19c32
Haley Pesik <haleypesik@Haleys-MacBook-Air.local>
2020-11-02 19:59:16 -0600

    Remove HelloWorld component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,19 +5,12 @@
       alt="Zach and Haley"
       src="../assets/images/DSC05091.jpg"
       class="image-center" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+  name: "Home"
 };
 </script>
 
