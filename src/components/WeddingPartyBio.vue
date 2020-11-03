676dcfc1ab2146d1fe4a36612d8876dd6a67b1c6
Haley Pesik <haleypesik@Haleys-MacBook-Air.local>
2020-11-02 21:53:09 -0600

    Create WeddingPartyBio

---
+++ src/components/WeddingPartyBio.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-card width="200px" tile>
+        <v-avatar size="100" style="position:absolute; top: 130px">
+            <v-img :src="imagePath"></v-img>
+        </v-avatar>
+        <v-list-item>
+            <v-list-item-content>
+            <v-list-item-title class="title">{{ name }}</v-list-item-title>
+            <v-list-item-subtitle>{{ position }}</v-list-item-subtitle>
+            </v-list-item-content>
+        </v-list-item>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'WeddingPartyBio',
+  props: {
+    name: String,
+    position: String,
+    biography: String,
+    imagePath: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/WeddingParty.vue
+++ src/views/WeddingParty.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="wedding-party">
     <h1>Wedding Party</h1>
+    <WeddingPartyBio 
+      name="Kylie Pesik"
+      position="Maid of Honor"
+      biography="Sister"
+      imagePath="DSC04258.jpg" />
   </div>
 </template>
 
 <script>
+import WeddingPartyBio from "@/components/WeddingPartyBio.vue";
+
 export default {
   name: "Wedding Party",
+  components: {
+    WeddingPartyBio,
+  },
 };
 </script>
