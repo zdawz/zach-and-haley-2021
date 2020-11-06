d0bd338a684ea4c6512c0d2798393a5c75db69a0
Haley Pesik <haleypesik@Haleys-MacBook-Air.local>
2020-11-05 21:56:48 -0600

    Update WeddingPartyBio

--- src/components/WeddingPartyBio.vue
+++ src/components/WeddingPartyBio.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-card width="200px" tile>
-        <v-avatar size="100" style="position:absolute; top: 130px">
-            <v-img :src="imagePath"></v-img>
+    <v-card width="280px" tile flat class="bio-card">
+        <v-avatar size="200">
+            <img :src='"../assets/images/bridal-party/" + imageName'/>
         </v-avatar>
         <v-list-item>
             <v-list-item-content>
-            <v-list-item-title class="title">{{ name }}</v-list-item-title>
-            <v-list-item-subtitle>{{ position }}</v-list-item-subtitle>
+            <v-list-item-title class="name">{{ name }}</v-list-item-title>
+            <v-list-item-subtitle class="position-text">{{ position }}</v-list-item-subtitle>
             </v-list-item-content>
         </v-list-item>
     </v-card>
@@ -19,11 +19,19 @@ export default {
     name: String,
     position: String,
     biography: String,
-    imagePath: String
+    imageName: String
   }
 }
 </script>
 
 <style scoped>
+.position-text {
+    text-transform: uppercase;
+}
 
+.name {
+  font-family: "Aesthete";
+  font-size: 50px !important;
+  overflow: unset !important;
+}
 </style>
