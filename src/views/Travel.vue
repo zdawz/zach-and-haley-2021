7a37e0388c9bfc6bba08198e37504aa8639f96ab
Haley Pesik <hpesik617@gmail.com>
2020-11-21 18:09:11 -0600

    Add text to travel page

--- src/views/Travel.vue
+++ src/views/Travel.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Travel</h1>
+    <h3>We are so excited to celebrate with you!</h3>
+    <h3>Here are our best picks for places to stay near our wedding venue.</h3>
   </div>
 </template>
 
