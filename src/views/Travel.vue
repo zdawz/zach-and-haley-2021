34cae413737094fac99945a394c4e8ccc39478d4
Haley Pesik <hpesik617@gmail.com>
2021-03-18 21:14:38 -0500

    Add hotels and info to travel page

--- src/views/Travel.vue
+++ src/views/Travel.vue
@@ -5,7 +5,7 @@
     <v-col class="column-padding">
       <h3>
         We are so excited to celebrate with you!
-        We have a couple recommendations for your stay near Kasota, MN.
+        We have a couple of recommendations for your stay near Kasota, MN.
       </h3>
     </v-col>
     
@@ -17,6 +17,31 @@
       <div class="travel-subtitle">Country Inn & Suites by Radisson</div>
       <div class="travel-subtitle">1900 Premier Drive, Mankato, MN 56001</div>
       <div class="travel-subtitle">507-388-8555</div>
+      <div class="travel-subtitle-info subtitle-info-padding">Standard Rooms & Studio Suites $124/night, 1BR Executive Suites $129/night</div>
+      <div class="travel-subtitle-info">Make reservations by phone and ask for the Pesik-Dawson wedding block</div>
+     
+    </v-card>
+
+    <img
+      src="../assets/images/travel/HolidayInnExpress.jpg"
+      class="image-center travel-image"/>
+
+    <v-card flat>
+      <div class="travel-subtitle">Holiday Inn Express & Suites</div>
+      <div class="travel-subtitle">2051 Adams Street, Mankato, MN 56001</div>
+      <div class="travel-subtitle">507-388-1880</div>
+      <v-btn
+        outlined
+        large
+        color="#2e2e2e"
+        href="http://www.hiexpress.com/mankatomn"
+        target="_blank"
+        class="detail-button">
+        Website
+      </v-btn>
+      <div class="travel-subtitle-info">1 King Bed $134.99/night, 2 Queen Beds $139.99/night</div>
+      <div class="travel-subtitle-info">For reservations by phone ask for the Pesik-Dawson wedding block</div>
+      <div class="travel-subtitle-info">For reservations on their website use group code: PES</div>
     </v-card>
   </div>
 </template>
@@ -44,6 +69,15 @@ export default {
   text-transform: uppercase;
 }
 
+.travel-subtitle-info {
+  font-family: "OpenSerif";
+  font-weight: normal;
+}
+
+.subtitle-info-padding {
+  padding-top: 16px;
+}
+
 .travel-button {
   justify-content: center;
   margin-left: auto;
@@ -56,5 +90,9 @@ export default {
   max-width: 840px;
   margin-left: auto;
   margin-right: auto;
+}
+
+.detail-button {
+  margin: 16px 16px 16px 16px;
 }
 </style>
